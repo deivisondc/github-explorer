a0ea6f59c542d801db45141901553da402c69807
Deivison Cardoso <deivisondc@gmail.com>
2020-11-15 22:33:26 -0300

    add repository list

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Container, Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Container, Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no GitHub</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="GitHub Explorer" />
+      <Title>Explore repositórios no GitHub</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="/">
+          <img
+            src="https://avatars2.githubusercontent.com/u/25139090?s=460&u=d313c1479cbc81452d740a9fc73f37fd0f9e7ac4&v=4"
+            alt="Deivison"
+          />
+          <div>
+            <strong>deivisondc/minesweeper</strong>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos
+              nisi optio officia et, quam inventore numquam quidem atque
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
